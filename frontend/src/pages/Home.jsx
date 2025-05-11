0e5600eacbf0ad0f7bd2165f82877d774dec8048
siddharthgumber07 <siddharthgumber123@gmail.com>
2025-05-11 09:45:58 +0530

    frontend started

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import Sidebar from "../layout/SideBar";
+import UserDashboard from "../components/UserDashboard"
+import AdminDashboard from "../components/AdminDashboard"
+import BookManagement from "../components/BookManagement";
+import Catalog from "../components/Catalog";
+import Users from "../components/Users";
+import MyBorrowedBooks from "../components/MyBorrowedBooks";
 const Home = () => {
-  return <></>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [selectedComponent, setSelectedComponent] = useState("");
+  const { user, isAuthenticated } = useSelector(state => state.auth);
+  // if(!isAuthenticated){
+  //   return <Navigate to={"/login"}/>
+  // }
+  return <>
+    <div className="relative md:pl-64 flex min-h-screen bg-gray-100">
+      <div className="md:hidden z-10 absolute right-6 top-4 sm:top-6 flex justify-center items-center bg-black rounded-md h-9 w-9 text-white">
+        <GiHamburgerMenu className="text-2xl" onClick={() => { setIsSidebarOpen(!isSidebarOpen) }} />
+      </div>
+      <Sidebar
+        isSideBarOpen={isSidebarOpen}
+        setIsSideBarOpen={setIsSidebarOpen}
+        setSelectedComponent={setSelectedComponent}
+      />
+      {
+          (()=>{
+            switch (selectedComponent) {
+              case "Dashboard":
+                return user?.role==="User"?(<UserDashboard/>):(<AdminDashboard/>)
+                break;
+              case "Books":
+                return <BookManagement />;
+                break;
+              case "Catalog":
+                if(user.role==="Admin"){
+                  return <Catalog/>;
+                  break;
+                }
+              case "Users":
+                if(user.role==="Admin"){
+                  return <Users/>;
+                  break;
+                }
+              case "My Borrowed Books":
+                if(user.role==="Admin"){
+                  return <MyBorrowedBooks/>;
+                  break;
+                }
+              default:
+                return user?.role==="User"?(<UserDashboard/>):(<AdminDashboard/>)
+                break;
+            }
+          } )()
+      }
+    </div>
+  </>;
 };
 
 export default Home;
